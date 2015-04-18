d2db911bc2b8fca85442d0dea56db0c20df2f419
Jobin Kurian <jobinkurian@outlook.com>
2015-04-18 16:24:22 +0530

    Initial Commit

---
+++ Theoria.TT.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Theoria.TT.Web.MvcApplication" Language="C#" %>
---
+++ Theoria.TT.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Theoria.TT.Web.Startup))]
+namespace Theoria.TT.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
